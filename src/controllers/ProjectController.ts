210d91a696dd37ef8238d2c413cf1a7a6ebfb9a9
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-10-10 22:12:53 -0500

    feat: enhance project creation with validation middleware

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,9 +1,18 @@
+import Project from "@/models/Project";
+import { ErrorLogger } from "@/utils/logger";
 import type { Request, Response } from "express";
 export class ProjectController {
   static getAllProjects = async (req: Request, res: Response) => {
     res.send("Obteniendo todos los proyectos");
   };
   static createProject = async (req: Request, res: Response) => {
-    res.send("Create a new project");
+    const project = new Project(req.body);
+    try {
+      await project.save();
+      res.status(201).send("Project created successfully");
+    } catch (error) {
+      ErrorLogger("Error creating project", { error });
+      res.status(500).send("Error creating project");
+    }
   };
 }
--- src/routes/proyectRouter.ts
+++ src/routes/proyectRouter.ts
@@ -1,9 +1,22 @@
-import { ProjectController } from "@/controllers/ProjectController";
 import { Router } from "express";
+import { body } from "express-validator";
+import { ProjectController } from "@/controllers/ProjectController";
+import { handleInputErrors } from "@/middlewares/validation";
 
 const router: Router = Router();
 
+router.post(
+  "/",
+  body("projectName")
+    .notEmpty()
+    .withMessage("El nombre del proyecto es obligatorio"),
+  body("clientName")
+    .notEmpty()
+    .withMessage("El nombre del cliente es obligatorio"),
+  body("description").notEmpty().withMessage("La descripción es obligatoria"),
+  handleInputErrors,
+  ProjectController.createProject,
+);
 router.get("/", ProjectController.getAllProjects);
-router.post("/", ProjectController.createProject);
 
 export default router;
