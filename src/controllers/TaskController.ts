efe12ca7c5f3ed8bb36b72ba6ec87cc050839ec6
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-11-16 20:16:27 -0500

    feat: update createTask method to use request body for projectId and handle task creation

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,16 +1,24 @@
 import Project from "@/models/Project";
+import Task from "@/models/Task";
 import logger from "@/utils/logger";
 import type { Request, Response } from "express";
 
 export class TaskController {
   static async createTask(req: Request, res: Response) {
-    const { projectId } = req.params;
+    const { projectId } = req.body;
     try {
       const project = await Project.findById(projectId);
       if (!project) {
         res.status(404).json({ message: "Project not found" });
         return;
       }
+      const taskData = new Task(req.body);
+      taskData.project = project.id;
+      project.tasks.push(taskData.id);
+      await taskData.save();
+      await project.save();
+      logger.success("Task created successfully");
+      res.status(201).json({ message: "Tarea creada exitosamente", taskData });
     } catch (error) {
       logger.error("Error creating task", { error });
       res.status(500).send("Error creando tarea");
